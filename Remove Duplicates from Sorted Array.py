58fea036bf288baf8f56c397d37235c57aedcba7
Dihad Hasan <130214955+Dihad@users.noreply.github.com>
2024-05-05 13:37:56 +0600

    Remove Duplicates from Sorted Array.py

--- Remove Duplicates from Sorted Array.py
+++ Remove Duplicates from Sorted Array.py
@@ -1,6 +1,11 @@
 nums = [0,0,1,1,1,2,2,3,3,4]
 new_nums = []
+count = 0
 for num in nums:
     if num not in new_nums:
         new_nums.append(num)
+    if num in new_nums:
+        count=count+1
+for i in range(count):
+    new_nums.append('_')
 print(new_nums)
